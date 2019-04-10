6f87338e7198eb1a4700e139c9ae511f3311e5e7
Stijn Janssen <stinow@gmail.com>
2019-04-10 13:54:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,16 +6,18 @@ self.addEventListener('activate', (ev) => {
 	console.log('Service worker is nu actief');
 });
 
-self.addEventListener('fetch', (ev) => {
+self.addEventListener('fetch', (ev) => { async function(){
 	
-	// Aanpassen request en doorsturen + op abboneren:
-	console.log(`Onderschept! Data = ${ev.request.url}`);
+		// Aanpassen request en doorsturen + op abboneren:
+		console.log(`Onderschept! Data = ${ev.request.method}`);
+		
+		ev.respondWith(
+		
+			/* Mooier is al dan niet met cache te werken, maar gaat nu over
+			   het concept van onderscheppen */
+			return fetch(ev.request);		
+		);
 	
-	ev.respondWith(
-	
-		/* Mooier is al dan niet met cache te werken, maar gaat nu over
-		   het concept van onderscheppen */
-		return fetch(ev.request);		
-	);
+   }
 	
 });
