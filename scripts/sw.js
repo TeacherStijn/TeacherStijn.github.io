15e35656e683adcd12402febb1163378c81faf94
Stijn Janssen <stinow@gmail.com>
2019-04-10 11:22:17 +0200

    SW aan het opbouwen

---
+++ scripts/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (ev) => {
+	console.log('Service worker installatie gedaan');
+});
+
+self.addEventListener('activate', (ev) => {
+	console.log('Service worker is nu actief');
+});
+
+self.addEventListener('fetch', (ev) => {
+	
+});
