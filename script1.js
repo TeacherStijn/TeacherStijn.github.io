72f6b759dc971805ff1d8b4ed30eaa807773214c
Stijn Janssen <stinow@gmail.com>
2020-07-17 12:50:14 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,9 +1,15 @@
 self.addEventListener('message', function(msg){
 	//setTimeout(
 	//	function(){
-			for (let i=0; i<msg.data.length; i++){
-				Atomics.add(msg.data,i,10);
-				self.postMessage('Bijgewerkt naar 10?');
+			for (let i=0; i<1000000; i++){
+				Atomics.add(msg.data,0,10);
+				
+				if (i%500) {
+					Atomics.store(msg.data,5,10);
+					console.log("Notified: " + Atomics.notify(5));
+				}
+				Atomics.wait();
+				self.postMessage('Bijgewerkt');
 			}
 	//	}, 10000
 	//);
--- script2.js
+++ script2.js
@@ -1,9 +1,15 @@
 self.addEventListener('message', function(msg){
 	//setTimeout(
 	//	function(){
-			for (let i=0; i<msg.data.length; i++) {
-				Atomics.add(msg.data,i,20);
-				self.postMessage('Bijgewerkt naar 20?');			
+			for (let i=0; i<1000000; i++){
+				Atomics.add(msg.data,0,20);
+				
+				if (i%700) {
+					Atomics.store(msg.data,10,20);
+					console.log("Notified: " + Atomics.notify(10));
+				}
+				Atomics.wait();
+				self.postMessage('Bijgewerkt');
 			}
 	//	}, 1000
 	//);
