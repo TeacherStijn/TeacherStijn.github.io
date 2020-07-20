9b6cc69f90914e937ac84ecd67667fc28fe206a1
Stijn Janssen <stinow@gmail.com>
2020-07-20 10:31:27 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,23 +1,12 @@
 self.addEventListener('message', function(msg){
 	let reeks = new Uint8Array(msg.data);
 
-	while (Atomics.load(reeks,0)==42){
+	/*	while (Atomics.load(reeks,0)==42){
 		// Doe niets, wacht tot data is gewijzigd
-	}
+	}*/
+
+	// Wacht maximaal 15sec op een wijziging in de array
+	Atomics.wait(reeks, 0, 15000);
 
 	self.postMessage(`Data gewijzigd naar: ` + Atomics.load(reeks,0));
-	/*//setTimeout(
-	//	function(){
-			for (let i=0; i<1000000; i++){
-				Atomics.add(msg.data,0,10);
-				
-				if (i%500) {
-					Atomics.store(msg.data,5,10);
-					console.log("Notified: " + Atomics.notify(5));
-				}
-				Atomics.wait();
-				self.postMessage('Bijgewerkt');
-			}
-	//	}, 10000
-	//);*/
 });
